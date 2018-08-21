547560eb5a40a7d87b0db0b007c665cab20ceaaa
JULIANCHO923 <julianhenao92@gmail.com>
2018-08-21 16:09:55 -0500

    Probando configuración Sonar

--- src/test/java/com/ceiba/adnjulianhenao/testIntegracionEntrada/TestInsertarVehiculo.java
+++ src/test/java/com/ceiba/adnjulianhenao/testIntegracionEntrada/TestInsertarVehiculo.java
@@ -1,9 +1,8 @@
 package com.ceiba.adnjulianhenao.testIntegracionEntrada;
 
-import static org.junit.Assert.fail;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,16 +11,23 @@ import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.ceiba.adnjulianhenao.servicio.ServicioVehiculo;
+
 
 @RunWith(SpringRunner.class)
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TestInsertarVehiculo {
 
+	@Autowired
+	ServicioVehiculo servicioVehiculo;
+	
 	@Test
 	@SqlGroup(@Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:preparandoBD.sql"))
 	public void test() {
-		fail("Not yet implemented");
+		
+		
+		
 	}
 
 }
