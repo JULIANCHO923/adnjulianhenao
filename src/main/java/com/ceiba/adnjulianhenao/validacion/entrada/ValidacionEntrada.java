fb5144ead70d121d52c83cc5c35817fbfb56e05b
JULIANCHO923 <julianhenao92@gmail.com>
2018-08-22 11:24:12 -0500

    solucionado error de h2 - problema de que al iniciar los test no lo hacia en orden, por tal motivo el metodo que preparaba la BD iniciaba de ultimo, generando error

--- src/main/java/com/ceiba/adnjulianhenao/validacion/entrada/ValidacionEntrada.java
+++
@@ -1,9 +0,0 @@
-package com.ceiba.adnjulianhenao.validacion.entrada;
-
-import com.ceiba.adnjulianhenao.modelo.ModeloVehiculo;
-
-public interface ValidacionEntrada {
-	
-	public void validar(ModeloVehiculo vehiculo);
-	
-}
--- src/main/java/com/ceiba/adnjulianhenao/validacion/salida/ValidacionSalida.java
+++
@@ -1,6 +0,0 @@
-package com.ceiba.adnjulianhenao.validacion.salida;
-
-public interface ValidacionSalida {
-	
-	public void validar(int idRegistro, String placa);
-}
